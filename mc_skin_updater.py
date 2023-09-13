10e26e59573b035e44c18938a7bbd6e152d86ca8
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-09-12 23:07:18 -0400

    added the rest of the comments

--- mc_skin_updater.py
+++ mc_skin_updater.py
@@ -5,9 +5,9 @@ def convert(oldSkin): # The function assumes that you already checked that this 
 	newSkin.paste(oldSkin, (0, 0)) # Paste in most of the skin itself
 
 	# Copying right to left leg
-	legUL = ImageOps.mirror(oldSkin.crop((4, 16, 4 + 4, 16 + 4)))
+	legUL = ImageOps.mirror(oldSkin.crop((4, 16, 4 + 4, 16 + 4))) # To top left
 	newSkin.paste(legUL, (20, 48))
-	legUR = oldSkin.crop((8, 16, 8 + 4, 16 + 4))
+	legUR = oldSkin.crop((8, 16, 8 + 4, 16 + 4)) # To top right
 	newSkin.paste(legUR, (24, 48))
 	legBL = ImageOps.mirror(oldSkin.crop((0, 20, 0 + 12, 20 + 12))) # To bottom left
 	newSkin.paste(legBL, (16, 52))
@@ -15,9 +15,9 @@ def convert(oldSkin): # The function assumes that you already checked that this 
 	newSkin.paste(legBR, (28, 52))
 
 	# Copying right to left arm
-	legUL = ImageOps.mirror(oldSkin.crop((44, 16, 44 + 4, 16 + 4)))
+	legUL = ImageOps.mirror(oldSkin.crop((44, 16, 44 + 4, 16 + 4))) # To top left
 	newSkin.paste(legUL, (36, 48))
-	legUR = ImageOps.mirror(oldSkin.crop((48, 16, 48 + 4, 16 + 4)))
+	legUR = ImageOps.mirror(oldSkin.crop((48, 16, 48 + 4, 16 + 4))) # To top right
 	newSkin.paste(legUR, (40, 48))
 	legBL = ImageOps.mirror(oldSkin.crop((40, 20, 40 + 12, 20 + 12))) # To bottom left
 	newSkin.paste(legBL, (32, 52))
